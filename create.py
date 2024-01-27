d5e6c9cf795055786891e0545b484801d008606d
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2024-01-26 23:39:09 -0700

    fixed some issues

--- create.py
+++ create.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from tensorflow import keras
 import numpy as np
 import os, copy
 import time
@@ -12,42 +11,30 @@ class Model_Init():
         print()
         self.path_to_file = tf.keras.utils.get_file('shakespeare.txt', url)
         vocab, text = self.read_and_decode()
-        ids = self.create_numeric_ids(vocab)
-        chars = self.chars_from_ids(ids)
-        self.create_targets(text, chars)
+        self.create_chars_from_vocab(vocab)
+        self.create_ids_from_chars(vocab)
+        self.create_chars_from_ids()
+        self.create_targets(text)
     
+    def create_chars_from_vocab(self, vocab):
+        self.chars = tf.strings.unicode_split(vocab, input_encoding="UTF-8")
+
     def text_from_ids(self, ids):
         return tf.strings.reduce_join(self.chars_from_ids(ids), axis=-1)
 
-    def chars_from_ids(self, ids):
+    def create_chars_from_ids(self):
         self.chars_from_ids = tf.keras.layers.StringLookup(
             vocabulary=self.ids_from_chars.get_vocabulary(), invert=True, mask_token=None)
-        chars = self.chars_from_ids(ids)
-        return chars
 
     def split_input_target(self, sequence):
-        
-        
-        print("<---SEQ--->", "\n")
-        print("    ",sequence, "\n")
-        print("<---SEQ--->", "\n")
-        
         input_text = sequence[:-1]
         target_text = sequence[1:]
-
-       
-        print("<--INPUT_TEXT-->", "\n")
+        print("    ",sequence, "\n")
         print("    ",input_text, "\n")
-        print("<--INPUT_TEXT-->", "\n")
-
-      
-        print("<--TARGET_TEXT-->", "\n")
         print("    ",target_text, "\n")
-        print("<--TARGET_TEXT-->", "\n")
-    
         return input_text, target_text
 
-    def create_targets(self, text, chars):
+    def create_targets(self, text):
         seq_length = 100
         all_ids = self.ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
         ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
@@ -55,11 +42,8 @@ class Model_Init():
         dataset = sequences.map(self.split_input_target)
         self.create_batches(dataset)
 
-    def create_numeric_ids(self, vocab):
-        chars = tf.strings.unicode_split(vocab, input_encoding="UTF-8")
-        self.ids_from_chars = tf.keras.layers.StringLookup(vocabulary=list(vocab), mask_token=None)
-        ids = self.ids_from_chars(chars)
-        return ids
+    def create_ids_from_chars(self, vocab):
+        self.ids_from_chars = tf.keras.layers.StringLookup(vocabulary=list(vocab), mask_token=None) 
 
     def read_and_decode(self):
         text = open(self.path_to_file, 'rb').read().decode(encoding='utf-8')
@@ -84,19 +68,17 @@ class Model_Init():
         chosen = False
         while(chosen != True):
             input_str = str(input())
-            if(input_str == "l" or input_str == "n"):
+            if(input_str == "l"):
                 print()
+                self.load_model(train_dataset)
+                chosen = True
+            elif(input_str == "n"):
+                print()
+                self.create_model(train_dataset)
                 chosen = True
             else:
                 print("Enter either l for load or n for new", "\n")
-        
-        if(input_str == "l"):
-            self.load_model(train_dataset)
-        elif (input_str == "n"):
-            self.create_model(train_dataset)
-        
-       
-    
+
     def test_run(self, dataset, model_instance):
         for input_batch, target_batch in dataset.take(5):
             batch_predictions = model_instance(input_batch)
@@ -117,7 +99,7 @@ class Model_Init():
 
     def load_model(self, dataset):
         print("Loading existing model..")
-        model_instance = keras.models.load_model('horatio_bot.keras')
+        model_instance = tf.keras.models.load_model('horatio_bot.keras')
         self.test_run(dataset, model_instance)
         loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
         model_instance.compile(optimizer='adam', loss=loss)
